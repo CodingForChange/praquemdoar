68ab31c6a9d28727a8118faeb039c4a71f9397af
Ale Borba <ale.alvesborba@gmail.com>
2014-03-22 20:12:58 -0300

    Inserindo a funçao de busca

--- app/views.py
+++ app/views.py
@@ -2,20 +2,36 @@ from flask import render_template, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user
 from flask.ext.login import current_user, login_required
 from app import app, db, lm
-from forms import NewsletterForm
+from forms import NewsletterForm, SearchForm
 from models import Newsletter
+
+
+@app.route('/search/<query>', methods=['GET', 'POST'])
+def search_results(query):
+    form = SearchForm()
+    result_doacao = Doacao.query.filter(Doacao.tags.like('%' + query + '%'))
+    result_ong = Ong.query.filter(Ong.nome.like('%' + query + '%'))
+    if form.validate_on_submit():
+        return redirect(url_for('search_results', query=form.search.data))
+    return render_template('search.html',
+                           query=query,
+                           results=results
+                           form=form)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NewsletterForm()
+    form_busca = SearchForm()
     if form.validate_on_submit():
         news = Newsletter(nome=form.nome.data,
                           email=form.email.data)
         db.session.add(news)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('index.html', form=form)
+    if form_busca.validate_on_submit():
+        return redirect(url_for('search_results', query=form.search.data))
+    return render_template('index.html', form=form, form_busca=form_busca)
 
 
 @lm.user_loader
