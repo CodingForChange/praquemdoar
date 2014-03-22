9c7a8033b8ab3ce4ebfc91b824494de17f4fc084
Ale Borba <ale.alvesborba@gmail.com>
2014-03-22 13:09:33 -0300

    Reorganizando arvore de arquivos e pastas

---
+++ flask/lib/python2.7/site-packages/TwitterAPI/__init__.py
@@ -0,0 +1,20 @@
+__title__ = 'TwitterAPI'
+__version__ = '2.1.10'
+__author__ = 'Jonas Geduldig'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Jonas Geduldig'
+
+
+try:
+	from .TwitterOAuth import TwitterOAuth
+	from .TwitterAPI import TwitterAPI, TwitterResponse, RestIterator, StreamingIterator
+	from .TwitterRestPager import TwitterRestPager
+except:
+	pass
+
+
+__all__ = [
+	'TwitterAPI',
+	'TwitterOAuth',
+	'TwitterRestPager'
+]
