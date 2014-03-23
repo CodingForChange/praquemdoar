6171a1a996c6888b2ac6d2c96d74574655faa312
Silvia Rodrigues da Silva <srodrigues@soyuz.com.br>
2014-03-23 00:01:05 -0300

    atualizacao

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -19,6 +19,82 @@ $(document).ready(function(){
 		});
 	});
 	
+	jQuery.validator.addMethod("cnpj", function(cnpj, element) {
+		cnpj = jQuery.trim(cnpj);// retira espaços em branco
+		// DEIXA APENAS OS NÚMEROS
+		cnpj = cnpj.replace('/','');
+		cnpj = cnpj.replace('.','');
+		cnpj = cnpj.replace('.','');
+		cnpj = cnpj.replace('-','');
+	      console.log(cnpj);
+		var numeros, digitos, soma, i, resultado, pos, tamanho, digitos_iguais;
+		digitos_iguais = 1;
+	      
+		if (cnpj.length < 14 && cnpj.length < 15){
+		   return false;
+		}
+		for (i = 0; i < cnpj.length - 1; i++){
+		   if (cnpj.charAt(i) != cnpj.charAt(i + 1)){
+		      digitos_iguais = 0;
+		      break;
+		   }
+		}
+	      
+		if (!digitos_iguais){
+		   tamanho = cnpj.length - 2
+		   numeros = cnpj.substring(0,tamanho);
+		   digitos = cnpj.substring(tamanho);
+		   soma = 0;
+		   pos = tamanho - 7;
+	      
+		   for (i = tamanho; i >= 1; i--){
+		      soma += numeros.charAt(tamanho - i) * pos--;
+		      if (pos < 2){
+			 pos = 9;
+		      }
+		   }
+		   resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+		   if (resultado != digitos.charAt(0)){
+		      return false;
+		   }
+		   tamanho = tamanho + 1;
+		   numeros = cnpj.substring(0,tamanho);
+		   soma = 0;
+		   pos = tamanho - 7;
+		   for (i = tamanho; i >= 1; i--){
+		      soma += numeros.charAt(tamanho - i) * pos--;
+		      if (pos < 2){
+			 pos = 9;
+		      }
+		   }
+		   resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+		   if (resultado != digitos.charAt(1)){
+		      return false;
+		   }
+		   return true;
+		}else{
+		   return false;
+		}
+	}, "Informe um CNPJ válido."); // Mensagem padrão
+	$("#cnpj").mask("99.999.999/9999-99");
+	$("#form_login").validate({
+		rules: {
+		   cnpj: {required: true, cnpj: true},
+		   senha: "required",
+                    confirmacao: {
+                      equalTo: "#senha"
+                    }
+		},
+		messages: {		   
+		   cnpj: { cnpj: 'CNPJ inválido'}
+		}
+		,submitHandler:function(form) {
+		   //$("#form-login").submit();
+		}
+	});
+	
+	
+	
 });// JavaScript Document
 
 formataTwitter = function()
